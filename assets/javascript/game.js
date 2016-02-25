84ad549a3c5a565a5a3a13ec401029c71a5522b4
Alvaro Martinez <Al@Alvaros-MBP.home>
2016-02-25 12:12:48 -0500

    Fixed new game issues. working on fighter name disapper when defeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,127 +1,221 @@
-//Global VARIABLES
-//======================================================
+window.onload = function(){
+
+
+newGame();
 
 
 
 
-var tyson = {
-
-	healthPoints: 100,
-	attackPower: 6,
-	counterAttackPower: 6
-}
-
-var ali = {
-
-	healthPoints : 100,
-	attackPower: 6,
-	counterAttackPower: 6
-}
-
-var george = {
-
-	healthPoints: 100,
-	attackPower: 6,
-	counterAttackPower: 6
-}
-
-var frazier = {
-	healthPoints: 100,
-	attackPower: 6,
-	counterAttackPower: 6
-}
-var moves = 0
-var wins = 0
-var player1 = []
-var player2 = []
-//FUNCTIONS
-//========================================================
-function winLose (){
-
-	if (player1.healthPoints <= 0){
-		alert(" YOU GOT KNOCKED OUT! ")
-		newGame()
-	}else if (player2.healthPoints <= 0 && wins < 2){
-		alert("NEXT OPPONENT!")
-		player2 = []
-		$('#fighter2').empty()
-		wins += 1
-	}else if(player2.healthPoints <=0 && wins == 2){
-		alert("You are the Champion!")
-		newGame()
-	}
 };
 
 
-
-
-
-
-function fightStatus(){
-
-
-	$("#fightStatus").empty()
-	$("#opponentStatus").empty()
-
-	player1.healthPoints
-	$("#fightStatus").append("<p>" + "health : " + player1.healthPoints + "</p>")
-
-	player2.healthPoints
-	$("#opponentStatus").append("<p>" + "health : " + player2.healthPoints + "</p>")
-}
-
-function fight(){
-	player1Attack = Math.floor(Math.random() * player1.attackPower )
-	player1.attackPower += 6
-
-	player2Attack = player2.counterAttackPower
-
-	player1.healthPoints -= player2Attack
-	player2.healthPoints -= player1Attack
-
-}
+//FUNCTIONS
+//========================================================
 
 function newGame() {
-	wins = 0
-	moves = 0 
-	player1 = []
-	player2 = []
+	wins = 0;
+	moves = 0; 
+	player1 = [];
+	player2 = [];
  $('#start').append('<div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2 fighter\" id=\"tyson\"> <img src=\"assets/images/tyson.jpg\"></div>')
  $('#start').append('<div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2 fighter\" id=\"ali\"><img src=\"assets/images/ali.jpg\"</div>')
  $('#start').append('<div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2 fighter\" id=\"george\"><img src=\"assets/images/GeorgeForeman.jpg\"></div>')
  $('#start').append('<div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2 fighter\" id=\"frazier\"><img src=\"assets/images/frazier.jpg\"></div>')
- $('#fightStatus').empty()
- $('#opponentStatus').empty()
  $('#choosenOne').empty()
  $('#fighter2').empty()
-
+ $('#fightStatus').empty()
+ $('#opponentStatus').empty()
   tyson = {
 
+  	name: "Tyson",
 	healthPoints: 100,
 	attackPower: 6,
 	counterAttackPower: 6
 		}
 
  ali = {
-
+ 	name: "Ali",
 	healthPoints : 100,
 	attackPower: 6,
 	counterAttackPower: 6
 }
 
 george = {
-
+	name: "GeorgeForeman",
 	healthPoints: 100,
 	attackPower: 6,
 	counterAttackPower: 6
 }
 
 frazier = {
+	name: "Frazier",
 	healthPoints: 100,
 	attackPower: 6,
 	counterAttackPower: 6
 }
 
+
+$('#tyson').click(function(){
+		if (moves == 0){
+		$('#choosenOne').html(this);
+		player1 = tyson
+		$("#fightStatus").html("<p>" + "health : " + player1.healthPoints + "</p>");
+		$("#yourFighter").html("Iron Mike Tyson")	
+		console.log("player1 :" + player1.name)
+		
+		
+		}else if (moves > 0 && player2.length == 0  && player1 != tyson){
+		 $('#fighter2').html(this);
+		 player2 = tyson 
+		 $("#opponentStatus").html("<p>" + "health : " + player2.healthPoints + "</p>")
+		 $('#yourOpponent').html("Iron Mike Tyson")
+		 console.log("player2 :" + player2.name)
+		 
+		}else{
+			alert("illegal move 1")
+		}
+		moves += 1
+		console.log("moves:" + moves)
+	});
+
+
+	$('#ali').click(function(){
+		if (moves == 0){
+			$('#choosenOne').html(this);
+			player1 = ali
+			$("#fightStatus").html("<p>" +"Mike Tyson" + "health : " + player1.healthPoints + "</p>");
+			$('#yourFighter').html("Muhammed Ali");
+			console.log("player1 :" + player1.name);
+		}else if(moves > 0 && player2.length == 0 && player1 != ali ){
+			$('#fighter2').html(this);
+			player2 = ali
+			$("#opponentStatus").html("<p>" + "health : " + player2.healthPoints + "</p>")
+			$("#yourOpponent").html("Muhammed Ali");
+			console.log("player2 :" + player2.name);
+		}else{
+			alert("illegal move 2")
+		}
+	
+		moves += 1
+		console.log("moves:" + moves)
+	});
+
+	$('#george').click(function(){
+		if (moves == 0){
+			$('#choosenOne').html(this);
+			player1 = george
+			$('#fightStatus').html("<p>" + "health : " + player1.healthPoints + "</p>");
+			$("#yourFighter").html("George Forman")
+			console.log("player1: " + player1.name)
+		}else if(moves > 0 && player2.length == 0 && player1 != george){
+			$('#fighter2').html(this);
+			player2 = george
+			$("#opponentStatus").html("<p>" + "health : " + player2.healthPoints + "</p>")
+			$("#yourOpponent").html("George Forman")
+			console.log("player2:" + player2.name)
+		}else{
+			alert("illegal move")
+		}
+		moves += 1
+		console.log("moves :" + moves)
+	});
+	
+	$('#frazier').click(function(){
+		if (moves ==0){
+			$('#choosenOne').html(this);
+			player1 = frazier
+			$("#fightStatus").html("<p>" + "health : " + player1.healthPoints + "</p>");
+			$("#yourFighter").html("Joe Frazier")
+			console.log("player1 :" + player1.name)
+		}else if( moves > 0 && player2.length == 0 && player1 != frazier){
+			$('#fighter2').html(this);
+			player2	= frazier
+			$("#opponentStatus").html("<p>" + "health : " + player2.healthPoints + "</p>")
+			$('#yourOpponent').html("George Foreman");
+			console.log("player2: " +  player2.name)
+		}else{
+			alert('illegal move')
+		}
+		moves += 1
+		console.log("moves :" + moves)
+	});
+
+	$('#button').click(function(){
+		
+		
+		fightStatus();
+		fight();
+		winLose();
+		
+	    
+	
+
+	});
+
+	function winLose (){
+
+	if (player1.healthPoints <= 0){
+		alert(" YOU GOT KNOCKED OUT! ")
+		newGame();
+	}else if (player2.healthPoints <= 0 && wins < 2){
+		alert("NEXT OPPONENT!")
+		player2 = []
+		$('#fighter2').empty()
+		$('#opponentStatus').empty()
+		wins += 1
+	}else if(player2.healthPoints <=0 && wins == 2){
+		alert("You are the Champion!")
+		newGame();
+	}
+};
+
+
+function fightStatus(){
+
+	if(moves == 0){
+		$("#fightStatus").html("<p>" + "health : " + player1.healthPoints + "</p>")
+	console.log("player1 health : " + player1.healthPoints)
+		$("#opponentStatus").html("<p>Choose your opponent</p>")
+
+	}else{
+		$("#fightStatus").html("<p>" + "health : " + player1.healthPoints + "</p>")
+	console.log("player1 health : " + player1.healthPoints)
+	
+	$("#opponentStatus").html("<p>" + "health : " + player2.healthPoints + "</p>")
+	console.log("player2 health:" + player2.healthPoints)
+
+	}
+	
+
+	
+	
+
+}
+
+
+
+function fight(){
+	player1Attack = Math.floor(Math.random() * player1.attackPower )
+	player1.attackPower += 6
+	console.log("player1 attack :" + player1Attack)
+	player2Attack = player2.counterAttackPower
+	console.log("player2 attack :" + player2Attack)
+		
+	if(isNaN(player1.healthPoints)){
+		$("#fightStatus").html("<p>Choose a Player</p>")
+	}else if(isNaN(player2.healthPoints)){
+		$("#opponentStatus").html("<p>Choose your opponent</p>")
+	}else if(isNaN(player1.healthPoints) && isNaN(player2.healthPoints) ){
+		$("#fightStatus").html("<p>Choose a Player</p>")
+		$("#opponentStatus").html("<p>Choose your opponent</p>")
+	}else{
+	player1.healthPoints -= player2Attack
+	player2.healthPoints -= player1Attack
+	}
+	console.log("player1 health:" + player1.healthPoints)
+	console.log("player2 health:" + player2.healthPoints)
+
+}
 
 
 };
@@ -134,89 +228,18 @@ frazier = {
 
 
 
-
-
-
-window.onload =function(){
+//LISTENERS RESPONSIBLE FOR MOVING AROUND AND CHOOSING PLAYER
 
 	
-$(document).ready(function(){	
-	
-	$('#tyson').click(function(){
-		if (moves == 0){
-		$('#choosenOne').append(this);
-		player1 = tyson
-		
-		
-		
-		}else if (moves > 0 && player2.length == 0  && player1 != tyson){
-		 $('#fighter2').append(this);
-		 player2 = tyson 
-		 
-		}else{
-			alert("illegal move 1")
-		}
-		moves += 1
-	});
 
 
-	$('#ali').click(function(){
-		if (moves == 0){
-			$('#choosenOne').append(this);
-			
-			player1 = ali
-		}else if(moves > 0 && player2.length == 0 && player1 != ali ){
-			$('#fighter2').append(this);
-			player2 = ali
-		}else{
-			alert("illegal move 2")
-		}
-	
-		moves += 1
-	});
 
-	$('#george').click(function(){
-		if (moves == 0){
-			$('#choosenOne').append(this);
-			
-			player1 = george
-		}else if(moves > 0 && player2.length == 0 && player1 != george){
-			$('#fighter2').append(this);
-			player2 = george
-		}else{
-			alert("illegal move")
-		}
-		moves += 1
-	});
-	
-	$('#frazier').click(function(){
-		if (moves ==0){
-			$('#choosenOne').append(this);
-			player1 = frazier
-			
-		}else if( moves > 0 && player2.length == 0 && player1 != frazier){
-			$('#fighter2').append(this);
-			player2	= frazier
-		}else{
-			alert('illegal move')
-		}
-		moves += 1
-	});
-
-	$('#button').click(function(){
-		
-		
-		fightStatus()
-		fight()
-		winLose()
-	    
-	
-
-	});
-	
+ 	
 
 
-});
 
-}
+
+
+
+
 
